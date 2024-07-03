426aa1ad7bebc68f7be95fea8d90249a4951f4a2
BrainStone <yannick@tedworld.de>
2024-07-03 07:08:36 +0200

    Added first source

---
+++ src/BrainMath.hpp
@@ -0,0 +1,6 @@
+#ifndef BRAINMATH_BRAINMATH_HPP
+#define BRAINMATH_BRAINMATH_HPP
+
+#include "BrainMath/concepts.hpp"
+
+#endif //BRAINMATH_BRAINMATH_HPP
---
+++ test/test_dummy.cpp
@@ -0,0 +1,4 @@
+#include "gtest/gtest.h"
+#include "BrainMath.hpp"
+
+TEST(X, X) {}
